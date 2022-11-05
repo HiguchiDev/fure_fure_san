0b069c6499fa575e12678a210a78957f82057d11
root <root@ik1-404-34172.vs.sakura.ne.jp>
2022-11-05 19:42:43 +0900

    本番用に設定変更

--- fure_fure_san/settings.py
+++ fure_fure_san/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,7 +72,10 @@ TEMPLATES = [
 ]
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static_local" ]
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "static"),
+os.path.join(BASE_DIR, "hello/static"),
+]
 
 STATIC_ROOT = '/usr/share/nginx/html/static'
 MEDIA_ROOT = '/usr/share/nginx/html/media'
@@ -127,8 +131,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
